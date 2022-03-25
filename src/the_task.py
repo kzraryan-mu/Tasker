29845a1f679e3683fd5f0a0b53e254b9c9173d2e
Md Kamruz Zaman Rana <mrkfw@mail.missouri.edu>
2022-03-25 14:57:42 -0500

    test udate

--- src/the_task.py
+++ src/the_task.py
@@ -1,28 +1,50 @@
 # libraries
-import pyodbc
 import pandas as pd
+import pyodbc
 
 # Initializing variables
-source_file = '/Users/mrkfw/Documents/GitHub/Tasker/src/source.txt'
-destination_file = '/Users/mrkfw/Documents/GitHub/Tasker/src/destination.txt'
-select_query_file = '/Users/mrkfw/Documents/GitHub/Tasker/src/select.sql'
-update_query_file = '/Users/mrkfw/Documents/GitHub/Tasker/src/update.sql'
-dsn = ''
+source_file = '\\msits-srv-pdsql\ApplicationTeamNetworkShare\GradeRoster\GradeRosterEmpty.txt'
+destination_file = '\\msits-srv-pdsql\ApplicationTeamNetworkShare\GradeRoster\GradeRosterFilled.txt'
+select_query_file = '\\msits-srv-pdsql\ApplicationTeamNetworkShare\GradeRoster\GradeTransmission.sql'
+update_query_file = '\\msits-srv-pdsql\ApplicationTeamNetworkShare\GradeRoster\UpdateGradeStatus.sql'
+dsn = 'RIME PROD'
 cols = ['ACAD_GROUP', 'TERM', 'STRM', 'SESSION_CODE', 'CLASS_NBR', 'CRSE_ID', 'SUBJECT', 'CATALOG_NBR', 'CLASS_SECTION',
         'DESCR', 'INSTRUCTOR_ID', 'GRADE', 'EMPLID', 'LAST_NAME', 'FIRST_NAME', 'GRD_RSTR_TYPE_SEQ', 'ACAD_CAREER']
 
 # Connecting to data server
 with pyodbc.connect('DSN={}'.format(dsn)) as connection:
+    # step0
+    # updating database by comparing source and destination file
+    source_df = pd.read_csv(source_file, sep='\t', dtype=str)
+    source_df.head()
+    destination_df = pd.read_csv(destination_file, sep='\t', dtype=str)
+    output0 = pd.merge(
+        source_df
+        , destination_df
+        , how='inner'
+        , on=['CLASS_NBR', 'EMPLID']
+        , suffixes=('', '_y')
+    )
+    if output0.count > 0:
+        class_nbr_emplid = []
+        for index, row in output0.iterrows():
+            class_nbr_emplid.append(row['CLASS_NBR'] + '_' + row['EMPLID'])
+        str_class_nbr_emplid = "','".join(class_nbr_emplid)
+        with open(update_query_file, 'r') as f:
+            update_sql = f.read()
+            update_sql.replace('CLASS_NBR_EMPLID', str_class_nbr_emplid)
+            cursor = connection.cursor()
+            cursor.execute(update_sql)
+            connection.commit()
+            cursor.close()
     # step1
     # reading the data from server
     with open(select_query_file, 'r') as f:
         select_sql = f.read()
     output1 = pd.read_sql(select_sql, connection)
 
-    #step2
-    #compare output1 with source.txt
-    source_df = pd.read_csv(source_file, sep='\t', dtype=str)
-    source_df.head()
+    # step2
+    # compare output1 with source.txt
     output2 = pd.merge(
         source_df
         , output1
@@ -31,18 +53,18 @@ with pyodbc.connect('DSN={}'.format(dsn)) as connection:
         , suffixes=('', '_y')
     )
 
-    #step3
-    #write into the destination file from the output2
+    # step3
+    # write into the destination file from the output2
     final_output = output2[cols]
     final_output['GRADE'] = output2['GRADE_y'].values
     final_output.to_csv(destination_file, sep='\t', index=False)
 
-    #step4
-    #update into the data server
-    with open(update_query_file, 'r') as f:
-        update_sql = f.read()
-        cursor = connection.cursor()
-        cursor.execute(update_sql)
-        connection.commit()
-        cursor.close()
+    # step4
+    # update into the data server
+    # with open(update_query_file, 'r') as f:
+    #    update_sql = f.read()
+    #    cursor = connection.cursor()
+    #    cursor.execute(update_sql)
+    #    connection.commit()
+    #    cursor.close()
 print('Grade Roaster Completed!')
--- src/update.sql
+++ src/update.sql
@@ -1,1 +1,32 @@
-UPDATE enrollment SET transmissionStatusId = 3, transmissionStatusDate = getdate() where enrollmentid in ();
+UPDATE enrollment set transmissionstatusid = 3, transmissionstatusdate = getdate()
+WHERE enrollmentid in (
+       SELECT enrollmentid from enrollment e
+       INNER JOIN offering o
+              ON o.offeringid = e.offeringid
+       INNER JOIN AcademicYearBlock ayb
+              ON ayb.AcademicYearBlockId = o.AcademicYearBlockId
+       INNER JOIN block b
+              ON b.BlockId = ayb.blockid
+       INNER JOIN student s
+              ON s.personid = e.personid
+       INNER JOIN course c
+              ON c.Courseid = o.courseid
+       INNER JOIN MUSemester m
+              ON m.MUSemesterId = b.muSemesterid
+       INNER JOIN AcademicYear ay
+              ON ay.AcademicYearId = ayb.AcademicYearId
+       INNER JOIN REF_MU_Enrollment_Courses rmc
+              ON rmc.TERM = m.TextAbbreviated + CASE WHEN m.MUSemesterId = 3
+                     THEN RIGHT(ay.text,4)
+                     ELSE LEFT(ay.text,4)
+                     END AND rmc.SESSION_CODE = b.myZou_Block
+                     AND rmc.[CATALOG_NBR] = c.UniversityCourseCode
+                     AND 'M' + Convert( varchar(4), CASE WHEN b.YearOfStudyId in (3, 4)
+                           THEN (SELECT yearofstudyid
+                                  FROM StudentYearOfStudy
+                                  WHERE AcademicYearid = ay.AcademicYearId AND personid = e.PersonId AND IsDeleted = 0)
+                           ELSE b.YearOfStudyId END) = rmc.CLASS_SECTION
+       WHERE rmc.CLASS_NBR = {{CLASS_NBR from row}}
+       AND s.[MUStudentNumber]  = {{EMPLID from row}}
+       AND e.TransmissionStatusId = 2
+)
