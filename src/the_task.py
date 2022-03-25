0b48369000f421fc9e228265312cccac50ccedf1
Md Kamruz Zaman Rana <mrkfw@mail.missouri.edu>
2022-03-25 15:46:43 -0500

    Update the_task.py

--- src/the_task.py
+++ src/the_task.py
@@ -3,10 +3,11 @@ import pandas as pd
 import pyodbc
 
 # Initializing variables
-source_file = '\\msits-srv-pdsql\ApplicationTeamNetworkShare\GradeRoster\GradeRosterEmpty.txt'
-destination_file = '\\msits-srv-pdsql\ApplicationTeamNetworkShare\GradeRoster\GradeRosterFilled.txt'
-select_query_file = '\\msits-srv-pdsql\ApplicationTeamNetworkShare\GradeRoster\GradeTransmission.sql'
-update_query_file = '\\msits-srv-pdsql\ApplicationTeamNetworkShare\GradeRoster\UpdateGradeStatus.sql'
+source_file = r'\\msits-srv-pdsql\ApplicationTeamNetworkShare\GradeRoster\GradeRosterEmpty.txt'
+destination_file = r'\\msits-srv-pdsql\ApplicationTeamNetworkShare\GradeRoster\GradeRosterFilled.txt'
+select_query_file = r'\\msits-srv-pdsql\ApplicationTeamNetworkShare\GradeRoster\GradeTransmission.sql'
+update_query_file = r'\\msits-srv-pdsql\ApplicationTeamNetworkShare\GradeRoster\UpdateGradeStatus.sql'
+updated_file = r'\\msits-srv-pdsql\ApplicationTeamNetworkShare\GradeRoster\updated.txt'
 dsn = 'RIME PROD'
 cols = ['ACAD_GROUP', 'TERM', 'STRM', 'SESSION_CODE', 'CLASS_NBR', 'CRSE_ID', 'SUBJECT', 'CATALOG_NBR', 'CLASS_SECTION',
         'DESCR', 'INSTRUCTOR_ID', 'GRADE', 'EMPLID', 'LAST_NAME', 'FIRST_NAME', 'GRD_RSTR_TYPE_SEQ', 'ACAD_CAREER']
@@ -19,13 +20,16 @@ with pyodbc.connect('DSN={}'.format(dsn)) as connection:
     source_df.head()
     destination_df = pd.read_csv(destination_file, sep='\t', dtype=str)
     output0 = pd.merge(
-        source_df
-        , destination_df
-        , how='inner'
+        destination_df
+        , source_df
+        , how='left'
         , on=['CLASS_NBR', 'EMPLID']
         , suffixes=('', '_y')
-    )
-    if output0.count > 0:
+        , indicator=True
+    ).dropna(subset=['CLASS_NBR', 'EMPLID'])
+    destination_df_2 = output0[output0['_merge'] == 'left_only']
+    destination_df_2[cols].to_csv(updated_file, sep='\t', index=False, header=False)
+    if destination_df_2.count > 0:
         class_nbr_emplid = []
         for index, row in output0.iterrows():
             class_nbr_emplid.append(row['CLASS_NBR'] + '_' + row['EMPLID'])
@@ -57,7 +61,7 @@ with pyodbc.connect('DSN={}'.format(dsn)) as connection:
     # write into the destination file from the output2
     final_output = output2[cols]
     final_output['GRADE'] = output2['GRADE_y'].values
-    final_output.to_csv(destination_file, sep='\t', index=False)
+    final_output.to_csv(destination_file, sep='\t', index=False, header=False)
 
     # step4
     # update into the data server
